751f60ab3937b5d37a7a8b04170f9705617e6ec4
Nora Top <top.nora@gmail.com>
2015-09-16 21:34:52 +0000

    add fortune-teller.js file that output a fortune to the console

---
+++ fortune-teller.js
@@ -0,0 +1,3 @@
+var fortune = require("/home/ubuntu/workspace/library/fortune");
+
+console.log(fortune.getFortune());
--- library/fortune.js
+++ library/fortune.js
@@ -1,9 +1,9 @@
-module.export = {
-    getFortune : getFortune
-}
-
 function getFortune() {
-    var fortunes = ['fortune1','fortune2','fortune3','fortune4','fortune5','fortune6','fortune7','fortune8'];
+    var fortunes = ["You'll feel much better once you've given up hope.","You have taken yourself too seriously.","Expect the worst, it's the least you can do.","You look tired.","And do you think (fop that I am) that I could be the Scarlet Pumpernickel?","Today is the last day of your life so far.","You will hear good news from one you thought unfriendly to you.","Are you making all this up as you go along?"];
     var fortuneIndex = Math.round((fortunes.length)*Math.random());
     return fortunes[fortuneIndex];
+}
+
+module.exports = {
+    getFortune : getFortune
 }
