cd9421731e120a60765f5036698b71986fad07e2
Nora Top <top.nora@gmail.com>
2015-09-16 22:47:13 +0000

    add the argument for the number of fortunes requested

--- fortune-teller.js
+++ fortune-teller.js
@@ -1,3 +1,7 @@
 var fortune = require("/home/ubuntu/workspace/library/fortune");
+var numberOfFortuneRequested = process.argv[2];
 
-console.log(fortune.getFortune());
+for (var i=0; i<numberOfFortuneRequested; i++)
+{
+    console.log(fortune.getFortune());
+}
