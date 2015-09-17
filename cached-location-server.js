0bf4c83295c0bac78a30f8ad430df0815e779622
Nora Top <top.nora@gmail.com>
2015-09-17 20:10:50 +0000

    upgraded cached-location-server to store cached location in an object instead of an array

--- cached-location-server.js
+++ cached-location-server.js
@@ -1,63 +1,71 @@
 var http = require('http');
 var request = require('request');
 
-var cachedLocations = [];
+var cachedLocations = {};
 
 var requestListener = function (req, response) {
 
     var locationInUrl = req.url.substring(1).toLowerCase();
     var APIKey = '&AIzaSyAd2WFdWK8DclKk_BHmEf4o1JQ4v1rk4_o';
+    
+    //console.log('locationInUrl: '+locationInUrl);
 
-    if (locationInUrl !== "" && locationInUrl !== 'favicon.ico'){
-        
-        var locationInCached = false;
-        
-        cachedLocations.forEach(function(element){
-            if (locationInUrl === element[0]) {
-                console.log('in cache');
+    if (locationInUrl !== 'favicon.ico'){
+        if (locationInUrl !== ""){
+            var output = '';
+            var locationData = [] ;
+            var requestUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + locationInUrl + "&" + APIKey;
+    
+            //console.log(cachedLocations);
+            //console.log('requestUrl: '+requestUrl);
+            //console.log('location in the cache ? ' + cachedLocations.hasOwnProperty(locationInUrl));
+            
+            if (cachedLocations.hasOwnProperty(locationInUrl)) {
+                //console.log('in cache');
+                locationData = cachedLocations[locationInUrl];
                 
-                locationInCached = true;
-                response.writeHead(200);
-                var geoLocation = element[1].geometry.location;
-                response.writeHead(200);
-                response.end('Cached response: This location has a latitude of ' + geoLocation.lat + ' and a longitude of ' + geoLocation.lng);    
-                return;
-            }
-        });
-        
-        console.log(locationInCached);
-
-        if(!locationInCached){
-            request('https://maps.googleapis.com/maps/api/geocode/json?address=' + locationInUrl + '&' + APIKey, function(err, res, body) {
-                            console.log('request');
-
-                if (err) {
-                response.writeHead(200);
-                response.end('There was an error:' + err);           
+                if (locationData) {
+                        var geoLocation = locationData.geometry.location;
+                        output = 'Cached response: This location has a latitude of ' + geoLocation.lat + ' and a longitude of ' + geoLocation.lng;
                 }
                 else {
-                    var data = JSON.parse(body);
-                    var firstResult = data.results[0];
-                    
-                    cachedLocations.push([locationInUrl,firstResult]);
-                    console.log('cachedLocations = ' + cachedLocations);
-    
-                    if (firstResult) {
-                        var geoLocation = firstResult.geometry.location;
-                        response.writeHead(200);
-                        response.end('Fresh result: This location has a latitude of ' + geoLocation.lat + ' and a longitude of ' + geoLocation.lng);           
+                    output = "Sorry this location can't be found";
+                }
+                
+                response.writeHead(200);
+                response.end(output);
+            }
+            else {
+                request(requestUrl, function(err, res, body) {
+                    //console.log('in request');
+                    if (err) {
+                    output = 'There was an error:' + err;           
                     }
                     else {
-                        response.writeHead(200);
-                        response.end("Sorry this location can't be found");                         
+                        var data = JSON.parse(body);
+                        locationData = data.results[0];
+                        
+                        cachedLocations[locationInUrl] = locationData;
+                        //console.log(cachedLocations);
+        
+                        if (locationData) {
+                            var geoLocation = locationData.geometry.location;
+                            output = 'Fresh result: This location has a latitude of ' + geoLocation.lat + ' and a longitude of ' + geoLocation.lng;
+                        }
+                        else {
+                            output = "Sorry this location can't be found";
+                        }
                     }
-                }
-            });
+                    //console.log(output);
+                    response.writeHead(200);
+                    response.end(output);
+                });
+            }
         }
-    }
-    else {
-        response.writeHead(200);
-        response.end("Enter a location in the URL");    
+        else {
+            response.writeHead(200);
+            response.end("Enter a location in the URL");
+        }
     }
 };
 
